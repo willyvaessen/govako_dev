f3a66108acf340b7e8aa6b888b55ebe4d6de1966
WillyVaessen <github@willyvaessen.nl>
2023-11-18 22:13:53 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,54 @@
+import os
+
+import mysql.connector
+from mysql.connector import Error
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+
+mydb_govako = mysql.connector.connect(
+    host="pi4B1",
+    database=os.getenv('DB_NAME'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_USER_PWD')
+)
+
+
+def test_connection():
+    try:
+        conn = mydb_govako
+
+        if conn.is_connected():
+            db_info = conn.get_server_info()
+            print("Connected to MySQL Sever version ", db_info)
+            cursor = conn.cursor()
+            cursor.execute("select database();")
+            record = cursor.fetchone()
+            print("You're connected to database: ", record)
+
+    except Error as e:
+        print("Error while connecting to MySQL", e)
+
+    finally:
+        if conn.is_connected():
+            cursor.close()
+            conn.close()
+            print("MySQL connection is closed")
+
+
+test_connection()
+
+
+#
+# # cursor = mydb_root.cursor()
+#
+# databases = 'SHOW DATABASES'
+# cursor.execute(databases)
+# for (databases) in cursor:
+#     print(f"Database found: {databases[0]}")
+#
+#
+# # print("Root DB" + str(mydb_root))
+# print("GoVaKo DB" + str(mydb_govako))
---
+++ test-db-connection.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const SECRETS = process.env;
+
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+    host:"pi4B1",
+    user:SECRETS.DB_USER,
+    password:SECRETS.DB_USER_PWD,
+    database: SECRETS.DB_NAME
+})
+
+let sql = "";
+
+conn.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected")
+    sql = "ALTER TABLE pltf_users ADD COLUMN user_id INT AUTO_INCREMENT PRIMARY KEY";
+    conn.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Result: Table created")
+        console.log("Result: " + result)
+    })
+});
+
+
+
+
+/*
+Create table:
+sql = "CREATE TABLE pltf_users (name VARCHAR(255), password CHAR(64))";
+
+Alter table:
+sql = "ALTER TABLE pltf_users ADD COLUMN user_id INT AUTO_INCREMENT PRIMARY KEY";
+
+ */
